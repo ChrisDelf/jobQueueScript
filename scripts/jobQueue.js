6813e2226348c7e5daacff8000f722a81c9fcf49
chrisdelf <chrisdel@protonmial.com>
2023-04-26 01:01:07 +0000

    basic job script is working

--- scripts/jobQueue.js
+++ scripts/jobQueue.js
@@ -6,25 +6,42 @@ const {downLoadSong} = require('../scripts/downLoadSong')
 
 const jobQueue = async () =>
 {   
-   
-    const job = await Song.findOne({
+    
+    const job = await Song.findAll({
         where: {status: "unfinished"}
     }
     )
- 
-   
+    console.log(job)
+    if (!job)
+    {
+        return
+    }
+    else
+    {
+        if (Array.isArray(job))
+        {
+            job.forEach(async (j) =>{
+            console.log("MANY JOBS HERE IS A JOB", j)
+            await downLoadSong(j)
+            })
+
+        }
+        else {
+
+        console.log("ONE JOBER EWREWR EW",job)
         await downLoadSong(job)
+        }
+            
+        
+    }
         
        
 
+setTimeout(jobQueue, 10000)
         
- 
-
-
 }
 
 jobQueue()
-    
     
 
 
